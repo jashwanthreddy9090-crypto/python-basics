c3285c2eb461abfab4d3b80c715f2f7460469a68
jashwanth9090 <jashwanthreddy9090@gmail.com>
2026-06-11 10:00:23 +0530

    Add files via upload

---
+++ third.py
@@ -0,0 +1,4 @@
+#length of a rectangle
+length=int(input("length of the rectangle in cm:"))
+breadth=int(input("breadth of the rectangle in cm:"))
+print("therefore area of rectangle:",length*breadth,"cm")
